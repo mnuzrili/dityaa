a0d9221c793b11084b1b7dc456827455d4895132
nuzril <muhammadnuzril2325@gmail.com>
2023-10-27 17:04:17 +0700

    add image and blur

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,20 @@
 // import React from "react";
 import "./App.css";
+import Stars from "./components/Stars";
 import Heading from "./components/Heading";
 import Hero from "./components/Hero";
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import Sapi from "./assets/img/sapihead.png";
 // import { loadSlim } from "tsparticles-slim";
 
 function App() {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
   return (
     <>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "transparent",
-            },
-          },
-          fpsLimit: 120,
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 1,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 50,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "star",
-              options: {
-                sides: 5,
-              },
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
       <div className="flex flex-col h-screen items-center justify-center">
+        <img className="md:w-24 w-16" src={Sapi} />
         <Heading />
         <Hero />
       </div>
+      <Stars />
     </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 // import React from 'react'
-import Sapi from "../assets/img/sapihead.png";
+import Dit from "../assets/img/didit.png";
 
 const Hero = () => {
   return (
     <>
-      <img className="md:w-1/3 w-2/5 " src={Sapi} />
+      <img className="md:w-1/3 w-4/5 blur-lg" src={Dit} />
       <p className="sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl text-md text-bold text-white">
         On Your Second Degree!
       </p>
