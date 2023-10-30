884aee7fcdaeea44d2d5d864b828b5cc25094233
nuzril <muhammadnuzril2325@gmail.com>
2023-10-30 09:31:59 +0700

    add env mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,40 @@ import Stars from "./components/Stars";
 import Heading from "./components/Heading";
 import Hero from "./components/Hero";
 import Envelope from "./components/Envelope";
-// import Music from "./components/Music";
+import Maintenance from "./components/Maintenance";
 import Footer from "./components/Footer";
 import Sapi from "./assets/img/sapihead.png";
 // import { loadSlim } from "tsparticles-slim";
 
+const env = "dev";
+
+const Main = () => {
+  return (
+    <>
+      <img className="md:w-24 w-16" src={Sapi} />
+      <Heading />
+      <Hero />
+      <Envelope />
+      <div className="relative h-32 w-full">
+        <div className="absolute inset-x-0 bottom-0 h-16">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
 function App() {
   return (
     <>
-      <div className={"flex flex-col items-center justify-center"}>
-        <img className="md:w-24 w-16" src={Sapi} />
-        <Heading />
-        <Hero />
-        <Envelope />
-        {/* <Music /> */}
-        <div className="relative h-32 w-full">
-          <div className="absolute inset-x-0 bottom-0 h-16">
-            <Footer />
-          </div>
-        </div>
+      <div
+        className={
+          env == "dev"
+            ? "flex flex-col items-center justify-center h-screen"
+            : "flex flex-col items-center justify-center"
+        }
+      >
+        {env == "dev" ? <Maintenance /> : Main()}
       </div>
       <Stars />
     </>
---
+++ src/components/Maintenance.jsx
@@ -0,0 +1,5 @@
+const Maintenance = () => {
+  return <h1 className="text-xl text-white">Maintenance !</h1>;
+};
+
+export default Maintenance;
