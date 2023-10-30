605519697b80ed025c28a365d744f3f47f464919
Nuzril <muhammadnuzril2325@gmail.com>
2023-10-30 21:41:06 +0700

    fix degree

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <>
       <img className="md:w-1/4 w-4/5" src={Dit} />
       <span className="font-custom mb-4 md:text-sm text-ss inline-flex items-center rounded-full bg-purple-50 bg-opacity-75 px-2 py-1 font-bold text-purple-700 ring-2 ring-inset ring-purple-700/10 relative ">
-        drh. ADITYA WIDYAPRAMITA, M.Vet
+        drh. ADITYA WIDYAPRAMITA, M.Sc
         <div className="sm:text-ss text-sss absolute inline-flex items-center justify-center w-12 h-7 font-bold text-white bg-red-500 bg-opacity-70 border-2 border-white rounded-xl -top-5 -right-2 dark:border-gray-700 animate-bounce">
           New
         </div>
